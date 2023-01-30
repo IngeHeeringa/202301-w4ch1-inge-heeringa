ad1d310777f5c1b408f22327664ecccc4317c566
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-30 22:00:08 +0100

    Create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "@fontsource/open-sans";
+import Button from "./components/Button/Button";
 import Info from "./components/Info/Info";
 
 const App = () => {
   return (
     <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
       <section className="controls">
         <Info number={0} />
+        <Button />
       </section>
     </>
   );
